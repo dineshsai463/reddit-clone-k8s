bff8af0d6dfac7e71199f34498b100b457829ace
dineshsai463 <115925417+dineshsai463@users.noreply.github.com>
2024-02-24 19:19:08 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "reddit" # Replace with your actual S3 bucket name
+    bucket = "dinesh-twitter-dev" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
